55de84bdc3f1d5644bfc6a3c7f0a0269769a2b7a
rezabarzgar <reza.barzgar99@gmail.com>
2023-01-26 20:01:02 -0500

    commit :)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import time
 
 from utils.utils import read_dat, min_edit_distance
 
 import nltk
-
+from nltk.corpus import wordnet
 
 def main():
     ok = nltk.download("wordnet")
@@ -10,8 +11,29 @@ def main():
         print("not ok")
         return
     tokens_pair = read_dat("data/missp.dat")
-    dist = min_edit_distance(tokens_pair[0][0], tokens_pair[0][1])
-    print(dist)
+    # dist = min_edit_distance(tokens_pair[0][0], tokens_pair[0][1])
+    # print(dist)
+    wn = wordnet.words(lang='eng')
+    print("good")
+    # find_distance_naive(wn, tokens_pair)
+    print(len(list(wn)))
+    # len(wn)
+    # for w in wn:
+    #     i += 1
+    #     print(w)
+    #     if i == 10:
+    #         break
+
+
+def find_distance_naive(dictionary, token_set):
+    start = time.time()
+    for i, word in enumerate(dictionary):
+        if i > 10:
+            break
+        for t in token_set:
+            _ = min_edit_distance(t[1], word)
+    end = time.time()
+    print(end-start)
 
 
 if __name__ == '__main__':
